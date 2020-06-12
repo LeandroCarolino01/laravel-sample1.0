0442c97d23928b00bb437475221f9b7e7c910f29
leandro <carolino.leandro@gmail.com>
2020-06-12 14:50:05 +0200

    update funcs added

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.app')
 
+@section('title')
+    Single todo {{ $todo->name}}
+@endsection
+
 @section('content')
+    <div class="container">
         <h1 class="text-center my-5">
-        {{ $todo->name }}
-        </h1>
-        <div class="card card-default">
-            <div class="card-header">
-                Details
+            {{ $todo->name }}
+            </h1>
+            <div class="card card-default">
+                <div class="card-header">
+                    Details
+                </div>
+                <div class="card-body">
+                    {{ $todo->description }}
+                </div>
+            
             </div>
-            <div class="card-body">
-                {{ $todo->description }}
-            </div>
-        </div>
+        <a href="/todos/{{ $todo->id }}/edit" class="btn btn-info btn-sm my-5">Edit</a>
+    </div>
 @endsection
