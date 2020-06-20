72b68510ff23090259bcfcdc9d919e33ff1e78ca
leandro <carolino.leandro@gmail.com>
2020-06-20 10:51:49 +0200

    feat(btn):add new btn

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -12,6 +12,11 @@
             @foreach($todos as $todo)
                     <li class="list-group-item">
                         {{ $todo->name }}
+
+                        @if(!$todo->completed)
+                        <a href="/todos/{{ $todo->id }}/complete" style="color: white" class="btn btn-warning btn-sm float-right">Complete</a>
+                        @endif
+                        
                         <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
                     </li>
             @endforeach
